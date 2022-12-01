8d1d98ce40ad6a794ffb0b9a5349accab08235f6
John W. Straub <jstraub@uw.edu>
2022-11-30 16:35:57 -0800

    work on graphics tutorial

--- Cartesian Plane Part 1/src/main/java/com/acmemail/judah/figures/SampleTarget001.java
+++ Cartesian Plane Part 1/src/main/java/com/acmemail/judah/figures/SampleTarget001.java
@@ -3,6 +3,8 @@ package com.acmemail.judah.figures;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
@@ -24,18 +26,21 @@ public class SampleTarget001 extends JPanel
     private final float		axisWeight			= 4;
     private final float		majorTicWeight		= 3;
     private final float		minorTicWeight	    = 2;
+    private final float     gridWeight          = 1;
+    private final Font      textFont            = 
+        new Font( "Monospace", Font.PLAIN, 10 );
     
     private final int		minorTicsPerUnit	= 10;
-    private final int		majorTicsPerUnit	= 1;
-    private final float		pixelsPerUnit		= 100;
+    private final int		majorTicsPerUnit	= 2;
+    private final float		pixelsPerUnit		= 75;
     
-    private final float     minorTicLen         = 20;
+    private final float     minorTicLen         = 7;
     private final float     majorTicLen         = 2 * minorTicLen;
     
-    private final int       leftMargin          = 40;
-    private final int       rightMargin         = 10;
-    private final int       topMargin           = 10;
-    private final int       bottomMargin        = 40;
+    private final int       leftMargin          = 60;
+    private final int       rightMargin         = 20;
+    private final int       topMargin           = 20;
+    private final int       bottomMargin        = 60;
     
 //    private Rectangle2D     workingRect;
     private float           gridWidth;
@@ -95,8 +100,11 @@ public class SampleTarget001 extends JPanel
         maxYco = minYco + gridHeight;
         centerYco = minYco + gridHeight / 2f;
         
+        drawGrid();
         drawMinorTics();
+        drawMajorTics();
         drawAxes();
+        drawText();
         
         // begin boilerplate
         gtx.dispose();
@@ -115,30 +123,129 @@ public class SampleTarget001 extends JPanel
         gtx.draw( yAxis );
     }
     
+    private void drawMajorTics()
+    {
+        gtx.setColor( ticColor );
+        gtx.setStroke( new BasicStroke( majorTicWeight ) );
+        float   ticDist     = pixelsPerUnit / majorTicsPerUnit;
+        
+        float   numXUnits   = gridWidth / pixelsPerUnit;
+        float   numXTicsNeg = (int)(numXUnits * majorTicsPerUnit / 2);
+        float   firstXco    = centerXco - (int)numXTicsNeg * ticDist;
+        float   yco1        = centerYco - majorTicLen / 2;
+        float   yco2        = yco1 + majorTicLen;
+        for ( float xco = firstXco ; xco <= maxXco ; xco += ticDist )
+        {
+            Line2D  tic     = new Line2D.Float( xco, yco1, xco, yco2 );
+            gtx.draw( tic );
+        }
+        
+        float   numYUnits   = gridHeight / pixelsPerUnit;
+        float   numYTicsPos = (int)(numYUnits * majorTicsPerUnit / 2);
+        float   firstYco    = centerYco - (int)numYTicsPos * ticDist;
+        float   xco1        = centerXco - majorTicLen / 2;
+        float   xco2        = xco1 + majorTicLen;
+        for ( float yco = firstYco ; yco <= maxYco ; yco += ticDist )
+        {
+            Line2D  tic     = new Line2D.Float( xco1, yco, xco2, yco );
+            gtx.draw( tic );
+        }
+    }
+        
     private void drawMinorTics()
     {
         gtx.setColor( ticColor );
         gtx.setStroke( new BasicStroke( minorTicWeight ) );
-        float   distPerTic  = gridWidth / pixelsPerUnit * minorTicsPerUnit;
-        float   ticYco1     = centerYco - minorTicLen / 2;
-        float   ticYco2     = ticYco1 + minorTicLen;
+        float   ticDist     = pixelsPerUnit / minorTicsPerUnit;
         
-        float   totalTics   = 
-            gridWidth / pixelsPerUnit * minorTicsPerUnit;
-        float   firstXco    = centerXco - totalTics / 2 * pixelsPerUnit;
-        for ( float xco = firstXco ; xco <= maxXco ;  xco += distPerTic )
+        float   numXUnits   = gridWidth / pixelsPerUnit;
+        int     numXTicsNeg = (int)(numXUnits * minorTicsPerUnit / 2);
+        float   firstXco    = centerXco - (int)numXTicsNeg * ticDist;
+        float   yco1        = centerYco - minorTicLen / 2;
+        float   yco2        = yco1 + minorTicLen;
+        for ( float xco = firstXco ; xco <= maxXco ; xco += ticDist )
         {
-            Line2D  tic = new Line2D.Float( xco, ticYco1, xco, ticYco2 );
+            Line2D  tic     = new Line2D.Float( xco, yco1, xco, yco2 );
             gtx.draw( tic );
         }
         
-        float   ticXco1     = centerXco - minorTicLen / 2;
-        float   ticXco2     = ticXco1 + minorTicLen;
-        float   firstYco    = centerYco - totalTics / 2 * pixelsPerUnit;
-        for ( float yco = firstYco ; yco <= maxYco ;  yco += distPerTic )
+        float   numYUnits   = gridHeight / pixelsPerUnit;
+        int     numYTicsPos = (int)(numYUnits * minorTicsPerUnit / 2);
+        float   firstYco    = centerYco - (int)numYTicsPos * ticDist;
+        float   xco1        = centerXco - minorTicLen / 2;
+        float   xco2        = xco1 + minorTicLen;
+        for ( float yco = firstYco ; yco <= maxYco ; yco += ticDist )
         {
-            Line2D  tic = new Line2D.Float( ticXco1, yco, ticXco2, yco );
+            Line2D  tic     = new Line2D.Float( xco1, yco, xco2, yco );
             gtx.draw( tic );
+        }
+    }
+    
+    private void drawGrid()
+    {
+        gtx.setColor( gridColor );
+        gtx.setStroke( new BasicStroke( gridWeight ) );
+        float   ticDist     = pixelsPerUnit / majorTicsPerUnit;
+        
+        float   numXUnits   = gridWidth / pixelsPerUnit;
+        float   numXTicsNeg = (int)(numXUnits * majorTicsPerUnit / 2);
+        float   firstXco    = centerXco - (int)numXTicsNeg * ticDist;
+        for ( float xco = firstXco ; xco <= maxXco ; xco += ticDist )
+        {
+            Line2D  tic     = new Line2D.Float( xco, minYco, xco, maxYco );
+            gtx.draw( tic );
+        }
+        
+        float   numYUnits   = gridHeight / pixelsPerUnit;
+        float   numYTicsPos = (int)(numYUnits * majorTicsPerUnit / 2);
+        float   firstYco    = centerYco - (int)numYTicsPos * ticDist;
+        for ( float yco = firstYco ; yco <= maxYco ; yco += ticDist )
+        {
+            Line2D  tic     = new Line2D.Float( minXco, yco, maxXco, yco );
+            gtx.draw( tic );
+        }
+    }
+    
+    private void drawText()
+    {
+        gtx.setColor( textColor );
+        gtx.setFont( textFont );
+        FontMetrics metrics = gtx.getFontMetrics();
+        float       ticDist = pixelsPerUnit / majorTicsPerUnit;
+        
+        float   numXUnits   = gridWidth / pixelsPerUnit;
+        float   numXTicsNeg = (int)(numXUnits * majorTicsPerUnit / 2);
+        float   firstXco    = centerXco - (int)numXTicsNeg * ticDist;
+        float   nextXUnit   = -numXTicsNeg / majorTicsPerUnit;
+        for ( float xco = firstXco ; xco <= maxXco ; xco += ticDist )
+        {
+            if ( nextXUnit != 0 )
+            {
+                String      label   = String.format( "%.1f", nextXUnit );
+                Rectangle2D rect    = metrics.getStringBounds( label, gtx );
+                float       baseXco = xco - (float)rect.getWidth() / 2;
+                float       baseYco = 
+                    centerYco + majorTicLen / 2 + (float)rect.getHeight();
+                gtx.drawString( label, baseXco, baseYco );
+            }
+            nextXUnit += 1.0 / majorTicsPerUnit;
+        }
+        
+        float   numYUnits   = gridHeight / pixelsPerUnit;
+        float   numYTicsPos = (int)(numYUnits * majorTicsPerUnit / 2);
+        float   firstYco    = centerYco - (int)numYTicsPos * ticDist;
+        float   baseXco     = centerXco + majorTicLen / 2 + 3;
+        float   nextYUnit   = numYTicsPos / majorTicsPerUnit;
+        for ( float yco = firstYco ; yco <= maxYco ; yco += ticDist )
+        {
+            if ( nextYUnit != 0 )
+            {
+                String      label   = String.format( "%.1f", nextYUnit );
+                Rectangle2D rect    = metrics.getStringBounds( label, gtx );
+                float       baseYco = yco + (float)rect.getHeight() / 3;
+                gtx.drawString( label, baseXco, baseYco );
+            }
+            nextYUnit -= 1.0 / majorTicsPerUnit;
         }
     }
 }
