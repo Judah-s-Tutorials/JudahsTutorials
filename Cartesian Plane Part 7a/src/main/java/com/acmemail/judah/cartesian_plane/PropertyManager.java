9adfe9ffddecaf98bafe728d8883659268e79f11
john <johstraub1954@gmail.com>
2023-01-17 17:41:46 -0800

    work on graphics tutorial

--- Cartesian Plane Part 7a/src/main/java/com/acmemail/judah/cartesian_plane/PropertyManager.java
+++ Cartesian Plane Part 7a/src/main/java/com/acmemail/judah/cartesian_plane/PropertyManager.java
@@ -5,28 +5,144 @@ package com.acmemail.judah.cartesian_plane;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 /**
+ * Manage the properties for this application.
+ * <p style="margin-left: 2em; max-width: 25em;font-weight: bold;">
+ * This is a singleton class
+ * implemented as an enum
+ * as recommended by
+ * <a href="https://www.w3schools.blog/java-singleton-design-pattern">
+ * W3schools
+ * </a>
+ * and <em>Effective Java</em> by Joshua Bloch.
+ * </p>
+ * The name of a property
+ * and its corresponding value
+ * are stored in a Properties object
+ * as String pairs.
+ * <p id="initialValues">
+ * <b>Establishing the Initial Values of Properties</b>
+ * </p>
+ * Upon instantiation
+ * the initial values of all properties
+ * are obtained by:
+ * <ol>
+ * <li>
+ * Checking the command line
+ * for property definitions
+ * using the java command line option <em>-Dname=value</em>.
+ * Note that these are <em>command line options</em>
+ * NOT <em>command line arguments</em>.
+ * Command line options 
+ * are positioned on the command line
+ * <em>before</em> the name of the executable class:<br>
+ * <span style="font-family: Monospace;">
+ * &nbsp;&nbsp;&nbsp;&nbsp;
+ * java -cp . -DaxisWeight=3 app.Main
+ * </span>
+ * <br>
+ * If the value of the property has not 
+ * been defined on the command line:
+ * </li>
+ * <li>
+ * The environment is searched.
+ * If the property is not found
+ * in the environment:
+ * </li>
+ * <li>
+ * If a user properties file
+ * has been configured
+ * the user properties file is searched.
+ * If the value of the property
+ * is not found in 
+ * the user properties file
+ * (or if no such file
+ * has been configured):
+ * </li>
+ * <li>
+ * The default configuration file 
+ * is searched.
+ * If, after searching the default configuration file,
+ * a value for the property
+ * has not been established:
+ * </li>
+ * <li>
+ * A default value will be applied.
+ * </li>
+ * </ol>
+ * <p>
+ * The user can listen for property changes
+ * by registering a listener with:
+ * </p>
+ * <ul>
+ * <li>
+ * {@linkplain #addPropertyChangeListener(PropertyChangeListener)}
+ * to listen for changes to all properties; and/or
+ * </li>
+ * <li>
+ * {@linkplain #addPropertyChangeListener(String, PropertyChangeListener)}
+ * to listen for changes to a specific property.
+ * </li>
+ * </ul> 
+ * 
  * @author Jack Straub
- *
+ * 
+ * @see #addPropertyChangeListener(PropertyChangeListener)
+ * @see #addPropertyChangeListener(String, PropertyChangeListener)
  */
 public enum PropertyManager
 {
+    /** The single instance of this class. */
     INSTANCE;
     
+    /** 
+     * The name of the default properties file.
+     */
     private static final String         appPropertiesName   = 
         "appProperties.ini";
     
+    /** Map of property names to property values. */
     private final Map<String,String>    propertyMap     = new HashMap<>();
+    /** 
+     * Property name/value pairs from the user's properties file.
+     * Used only during initialization.
+     */
     private final Properties            userProperties  = new Properties();
+    /** 
+     * Property name/value pairs from the default properties file.
+     * Used only during initialization.
+     */
     private final Properties            appProperties   = new Properties();
     
+    /** List of property listeners. */
+    private final List<PropertyChangeListener>  propertyListeners   =
+        new ArrayList<>();
+    /** Collection of per-property listeners. */
+    private final Map<String,List<PropertyChangeListener>>
+        perPropertyListeners    = new HashMap<>();
+    
+    /**
+     * Constructor.
+     * Private as required by enums 
+     * and the singleton programming pattern.
+     * Establishes the initial values
+     * of all properties.
+     * 
+     * @see <a href="#initialValues">
+     *      Establishing the Initial Values of Properties
+     *      </a>
+     */
     private PropertyManager()
     {
         String  val = null;
@@ -54,6 +170,71 @@ public enum PropertyManager
     }
     
     /**
+     * Registers a property-change listener for changes
+     * to all properties.
+     * 
+     * @param listener  listener to register
+     */
+    public void addPropertyChangeListener( PropertyChangeListener listener )
+    {
+        propertyListeners.add( listener );
+    }
+    
+    /**
+     * Removes a listener from the registry
+     * of property-change listeners
+     * for all properties.
+     * 
+     * @param listener  listener to remove
+     */
+    public void 
+    removePropertyChangeListener( PropertyChangeListener listener )
+    {
+        propertyListeners.remove( listener );
+    }
+    
+    /**
+     * Registers a property-change listener for changes
+     * to a given property.
+     * 
+     * @param property  the name of the given property
+     * @param listener  the listener to register
+     */
+    public void addPropertyChangeListener( 
+        String                  property, 
+        PropertyChangeListener  listener
+    )
+    {
+        List<PropertyChangeListener>    list    = 
+            perPropertyListeners.get( property );
+        if ( list == null )
+        {
+            list = new ArrayList<PropertyChangeListener>();
+            perPropertyListeners.put( property, list );
+        }
+        propertyListeners.add( listener );
+    }
+    
+    /**
+     * Removes a listener from the registry
+     * of property-change listeners
+     * for a given properties.
+     * 
+     * @param property  the name of the given property
+     * @param listener  the listener to remove
+     */
+    public void removePropertyChangeListener( 
+        String                  property, 
+        PropertyChangeListener  listener
+    )
+    {
+        List<PropertyChangeListener>    list    = 
+            perPropertyListeners.get( property );
+        if ( list != null )
+            list.remove( listener );
+    }
+    
+    /**
      * Gets the value associated with a given property name
      * and returns it as an Integer.
      * 
@@ -71,6 +252,26 @@ public enum PropertyManager
         if ( sVal != null )
             iVal = Integer.parseInt( sVal );
         return iVal;
+    }
+    
+    /**
+     * Sets the value of a given property. 
+     * The previous value of the property is returned
+     * as type <em>Object</em>;
+     * if the property does not have a previous value
+     * null is returned.
+     * 
+     * @param propName  the given property name
+     * @param propVal   the property value
+     * 
+     * @return  the previous value of the property,
+     *          or null if none
+     */
+    public Object setProperty( String propName, int propVal )
+    {
+        String  sVal    = Integer.toString( propVal );
+        Object  oVal    = propagatePropertyChange( propName, sVal );
+        return oVal;
     }
     
     /**
@@ -94,6 +295,26 @@ public enum PropertyManager
     }
     
     /**
+     * Sets the value of a given property. 
+     * The previous value of the property is returned
+     * as type <em>Object</em>;
+     * if the property does not have a previous value
+     * null is returned.
+     * 
+     * @param propName  the given property name
+     * @param propVal   the property value
+     * 
+     * @return  the previous value of the property,
+     *          or null if none
+     */
+    public Object setProperty( String propName, float propVal )
+    {
+        String  sVal    = Float.toString( propVal );
+        Object  oVal    = propagatePropertyChange( propName, sVal );
+        return oVal;
+    }
+    
+    /**
      * Gets the value associated with a given property name
      * and returns it as a Boolean.
      * 
@@ -110,6 +331,45 @@ public enum PropertyManager
         return bVal;
     }
     
+    /**
+     * Sets the value of a given property. 
+     * The previous value of the property is returned
+     * as type <em>Object</em>;
+     * if the property does not have a previous value
+     * null is returned.
+     * 
+     * @param propName  the given property name
+     * @param propVal   the property value
+     * 
+     * @return  the previous value of the property,
+     *          or null if none
+     */
+    public Object setProperty( String propName, boolean propVal )
+    {
+        String  sVal    = Boolean.toString( propVal );
+        Object  oVal    = propagatePropertyChange( propName, sVal );
+        return oVal;
+    }
+    
+    /**
+     * Sets the value of a given property. 
+     * The previous value of the property is returned
+     * as type <em>Object</em>;
+     * if the property does not have a previous value
+     * null is returned.
+     * 
+     * @param propName  the given property name
+     * @param propVal   the property value
+     * 
+     * @return  the previous value of the property,
+     *          or null if none
+     */
+    public Object setProperty( String propName, String propVal )
+    {
+        Object  oVal    = propagatePropertyChange( propName, propVal );
+        return oVal;
+    }
+
     /**
      * Gets the value associated with a given property name
      * and returns it as a Color.
@@ -133,6 +393,30 @@ public enum PropertyManager
         return cVal;
     }
     
+    /**
+     * Sets the value of a given Color property. 
+     * The given value is converted to an integer
+     * by calling <em>Color.getRGB()</em>
+     * then the integer becomes the stored value.
+     * The previous value of the property is returned
+     * as type <em>Object</em>;
+     * if the property does not have a previous value
+     * null is returned.
+     * 
+     * @param propName  the given property name
+     * @param propVal   the property value
+     * 
+     * @return  the previous value of the property,
+     *          or null if none
+     */
+    public Object setProperty( String propName, Color propVal )
+    {
+        int     cVal    = propVal.getRGB();
+        String  sVal    = Integer.toString( cVal );
+        Object  oVal    = propagatePropertyChange( propName, sVal );
+        return oVal;
+    }
+
     /**
      * Convert the value associated with a given property name
      * to a font style return the result.
@@ -243,51 +527,6 @@ public enum PropertyManager
      * This method is called to get the initial value
      * of a named property.
      * The value is obtained by:
-     * <ol>
-     * <li>
-     * Checking the command line
-     * for property definitions
-     * using the java command line option <em>-Dname=value</em>.
-     * Note that these are <em>command line options</em>
-     * NOT <em>command line arguments</em>.
-     * Command line options 
-     * are positioned on the command line
-     * <em>before</em> the name of the executable class:<br>
-     * <span style="font-family: Monospace;">
-     * &nbsp;&nbsp;&nbsp;&nbsp;
-     * java -cp . -DaxisWeight=3 app.Main
-     * </span>
-     * <br>
-     * If the value of the property has not 
-     * been defined on the command line:
-     * </li>
-     * <li>
-     * The environment is searched.
-     * If the property is not found
-     * in the environment:
-     * </li>
-     * <li>
-     * If a user properties file
-     * has been configured
-     * the user properties file is searched.
-     * If the value of the property
-     * is not found in 
-     * the user properties file
-     * (or if no such file
-     * has been configured):
-     * </li>
-     * <li>
-     * The default configuration file 
-     * is searched.
-     * If, after searching the default configuration file,
-     * a value for the property
-     * has not been established:
-     * </li>
-     * <li>
-     * The given default (parameter <em>defValue</em>)
-     * will be applied. 
-     * </li>
-     * </ol>
      * @param propName  property name
      * @param defValue  default value to be applied if necessary
      * 
@@ -308,5 +547,29 @@ public enum PropertyManager
             val = defValue;
         
         return val;
+    }
+    
+    private Object propagatePropertyChange( String propName, String newVal )
+    {
+        Object  oldVal  = propertyMap.put( propName, newVal );
+        PropertyChangeEvent eventObj    = 
+            new PropertyChangeEvent(
+                INSTANCE,
+                propName,
+                oldVal,
+                newVal
+            );
+        for ( PropertyChangeListener listener : propertyListeners )
+            listener.propertyChange( eventObj );
+        
+        List<PropertyChangeListener>    list    = 
+            perPropertyListeners.get( propName );
+        if ( list != null )
+        {
+            for ( PropertyChangeListener listener : propertyListeners )
+                listener.propertyChange( eventObj );
+        }
+        
+        return oldVal;
     }
 }
