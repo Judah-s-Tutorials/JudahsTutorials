509c148b4779a8ffdde6171d79aef3359a81df22
john <johstraub1954@gmail.com>
2023-04-29 15:06:00 -0700

    work on cartesian plane lesson 12

--- JEPSandbox/src/main/java/com/acmemail/judah/cartesian_plane/jep_sandbox/Investigator.java
+++ JEPSandbox/src/main/java/com/acmemail/judah/cartesian_plane/jep_sandbox/Investigator.java
@@ -1,115 +1,22 @@
 package com.acmemail.judah.cartesian_plane.jep_sandbox;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 import org.nfunk.jep.JEP;
-import org.nfunk.jep.ParseException;
-import org.nfunk.jep.type.Complex;
 
 public class Investigator
 {
-    private final BufferedReader    reader;
-    private final JEP               parser;
-    private       String            input;
-    
     public static void main(String[] args)
     {
-        try ( 
-            InputStreamReader inReader    = new InputStreamReader( System.in );
-            BufferedReader bufReader = new BufferedReader( inReader );
-        )
-        {
-            Investigator    app = new Investigator( bufReader );
-            app.exec();
-        }
-        catch ( IOException exc )
-        {
-            exc.printStackTrace();
-            System.exit( 1 );
-        }
-    }
-
-    private Investigator( BufferedReader reader )
-    {
-        this.reader = reader;
-        parser = new JEP();
+        JEP parser = new JEP();
         parser.addStandardConstants();
         parser.addStandardFunctions();
         parser.addComplex();
         parser.setImplicitMul( true );
-    }
-    
-    private void exec() throws IOException
-    {
-        String  prompt  = "Enter a command: ";
-        System.out.print( prompt );
-        input = reader.readLine().trim();
-        while ( !input.equalsIgnoreCase( "exit" ) )
-        {
-            if ( input.startsWith( "set" ) )
-                set();
-            else if ( input.startsWith( "exit" ) )
-                ;
-            else
-                parse();
-            System.out.print( prompt );
-            input = reader.readLine().trim();
-        } 
-    }
-    
-    private void parse()
-    {
-        try
-        {
-            parser.parse( input );
-            if ( parser.hasError() )
-                System.out.println( parser.getErrorInfo() );
-            else
-                System.out.println( parser.getComplexValue() );
-        }
-        catch ( ParseException exc )
-        {
-            System.out.println( exc.getErrorInfo() );
-        }
-
-    }
-    
-    private void set()
-    {
-        String          name    = "";
-        String          strVal  = "";
-        StringTokenizer tizer   = new StringTokenizer( input );
-        // throw away "set"
-        tizer.nextToken();
-        // validate 
-        if ( !tizer.hasMoreElements() )
-            System.out.println( "Invalid input: " + input );
+        
+        parser.addVariable( "z", 3, 2 );
+        parser.parseExpression( "2 * z" );
+        if ( parser.hasError() )
+            System.out.println( parser.getErrorInfo() );
         else
-        { 
-            try
-            {
-                name = tizer.nextToken();
-                strVal = tizer.hasMoreTokens() ? tizer.nextToken( "" ) : "0";
-                parser.parse( strVal.trim() );
-                if ( parser.hasError() )
-                    System.out.println( parser.getErrorInfo() );
-                else
-                {
-                    Complex cpx = parser.getComplexValue();
-                    parser.addVariable( name,  parser.getValue() );
-                    if ( parser.hasError() )
-                        System.out.println( parser.getErrorInfo() );
-                    else
-                        System.out.println( name + ": " + parser.getValue() );
-                }
-            }
-            catch ( ParseException exc )
-            {
-                System.out.println( exc.getErrorInfo() );
-            }
-        }
+            System.out.println( parser.getComplexValue() );
     }
 }
---
+++ JEPSandbox/src/main/java/com/acmemail/judah/cartesian_plane/jep_sandbox/InvestigatorTrig1.java
@@ -0,0 +1,41 @@
+package com.acmemail.judah.cartesian_plane.jep_sandbox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import org.nfunk.jep.JEP;
+import org.nfunk.jep.ParseException;
+import org.nfunk.jep.type.Complex;
+
+public class InvestigatorTrig1
+{
+    private static final    JEP parser = new JEP();
+    public static void main(String[] args)
+    {
+        parser.addStandardConstants();
+        parser.addStandardFunctions();
+        parser.addComplex();
+        parser.setImplicitMul( true );
+        
+        parser.addVariable( "z", 3, 2 );
+        print( "2 * 3" );
+        print( "2z" );
+        print( "2(3 + 2i)" );
+        print( "cos(z)" );
+        
+        Complex cpx = new Complex( 3, 2 );
+        System.out.println( cpx.cos() );
+    }
+    
+    private static void print( String expr )
+    {
+        parser.parseExpression( expr );
+        if ( parser.hasError() )
+            System.out.println( parser.getErrorInfo() );
+        else
+            System.out.println( parser.getComplexValue() );
+    }
+}
+;
