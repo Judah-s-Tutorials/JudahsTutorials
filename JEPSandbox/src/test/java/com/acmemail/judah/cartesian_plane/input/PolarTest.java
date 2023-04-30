6a4fe5fff0d8edb357ced5205b82b179e7ed0bc8
john <johstraub1954@gmail.com>
2023-04-30 15:36:10 -0700

    work on cartesian plane lesson 12

---
+++ JEPSandbox/src/test/java/com/acmemail/judah/cartesian_plane/input/PolarTest.java
@@ -0,0 +1,119 @@
+package com.acmemail.judah.cartesian_plane.input;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.awt.geom.Point2D;
+
+import org.junit.jupiter.api.Test;
+import org.nfunk.jep.type.Complex;
+
+class PolarTest
+{
+    private static final double testRadius  = 2;
+    private static final double testTheta   = Math.PI / 3;
+
+    private static final double testXco     = 
+        testRadius * Math.cos( testTheta );
+    private static final double testYco     = 
+        testRadius * Math.sin( testTheta );
+    
+    private static final Polar      testPolar   = 
+        Polar.of( testRadius, testTheta );
+    private static final Complex    testComplex = 
+        new Complex( testXco, testYco );
+    private static final Point2D    testPoint   = 
+        new Point2D.Double( testXco, testYco );
+    
+    @Test
+    void testToComplex()
+    {
+        Complex cpx = testPolar.toComplex();
+        assertEquals( testXco, cpx.re(), .00001 );
+        assertEquals( testYco, cpx.im(), .00001 );
+    }
+
+    @Test
+    void testToPoint()
+    {
+        Point2D point   = testPolar.toPoint();
+        assertEquals( testXco, point.getX(), .00001 );
+        assertEquals( testYco, point.getY(), .00001 );
+    }
+
+    @Test
+    void testGetRadius()
+    {
+        assertEquals( testRadius, testPolar.getRadius() );
+    }
+
+    @Test
+    void testGetTheta()
+    {
+        assertEquals( testTheta, testPolar.getTheta() );
+    }
+
+    @Test
+    void testToString()
+    {
+        int     maxLen          = 4;
+        String  testRadiusStr   = Double.toString( testRadius );
+        if ( testRadiusStr.length() > maxLen )
+            testRadiusStr = testRadiusStr.substring( 0, maxLen );
+        String  testThetaStr    = Double.toString( testTheta );
+        if ( testThetaStr.length() > maxLen )
+            testThetaStr = testThetaStr.substring( 0, maxLen );
+        
+        String  testStr = testPolar.toString();
+        assertTrue( testStr.contains( testRadiusStr ), testRadiusStr );
+        assertTrue( testStr.contains( testThetaStr ), testThetaStr );
+    }
+
+    @Test
+    void testOfComplex()
+    {
+        Polar   actPolar    = Polar.of( testComplex );
+        assertEquals( testPolar.getRadius(), actPolar.getRadius(), .0001 );
+        assertEquals( testPolar.getTheta(), actPolar.getTheta(), .0001 );
+    }
+
+    @Test
+    void testOfPoint2D()
+    {
+        Polar   actPolar    = Polar.of( testPoint );
+        assertEquals( testPolar.getRadius(), actPolar.getRadius(), .0001 );
+        assertEquals( testPolar.getTheta(), actPolar.getTheta(), .0001 );
+    }
+
+    @Test
+    void testOfDoubleDouble()
+    {
+        Polar   actPolar    = Polar.of( testRadius, testTheta );
+        assertEquals( testRadius, actPolar.getRadius() );
+        assertEquals( testTheta, actPolar.getTheta() );
+    }
+
+    @Test
+    void testOfXY()
+    {
+        Polar   actPolar    = Polar.of( testRadius, testTheta );
+        assertEquals( testRadius, actPolar.getRadius(), .0001 );
+        assertEquals( testTheta, actPolar.getTheta(), .0001 );
+    }
+
+    @Test
+    void testRadiusOfXY()
+    {
+        Double  actRadius   = Polar.radiusOfXY( testXco, testYco );
+        assertEquals( testRadius, actRadius );
+    }
+
+    @Test
+    void testThetaOfXY()
+    {
+        Double  actTheta    = Polar.thetaOfXY( testXco, testYco );
+        assertEquals( testTheta, actTheta );
+    }
+
+}
