f876296b668a271b611024e9dc969a5e09c1495e
john <johstraub1954@gmail.com>
2024-01-10 15:03:05 -0800

    work on lesson 16

---
+++ Cartesian Plane Part 17/src/main/java/com/acmemail/judah/cartesian_plane/App.java
@@ -0,0 +1,16 @@
+package com.acmemail.judah.cartesian_plane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( System.getProperties().get( "java.home" ) );
+        System.out.println( System.getProperties().get( "java.vendor" ) );
+        System.out.println( System.getProperties().get( "java.version" ) );
+        System.out.println( System.getProperties().get( "java.class.path" ) );
+    }
+}
