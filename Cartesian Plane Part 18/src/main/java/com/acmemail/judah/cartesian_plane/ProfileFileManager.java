9bc11aa67202470dc67fb97bb74d0f0a815589f4
john <johstraub1954@gmail.com>
2024-10-26 13:43:44 -0700

    Work on ProfileFileManager

--- Cartesian Plane Part 18/src/main/java/com/acmemail/judah/cartesian_plane/ProfileFileManager.java
+++ Cartesian Plane Part 18/src/main/java/com/acmemail/judah/cartesian_plane/ProfileFileManager.java
@@ -12,9 +12,41 @@ import javax.swing.JOptionPane;
 
 public class ProfileFileManager
 {
-    private final  JFileChooser chooser;
-    private File     currFile            = null;
-    private boolean  lastResult          = false;
+    /** 
+     * Represents the operator's choice of Save or Open 
+     * in the file chooser dialog.
+     * @see #getLastAction()
+     */
+    public static final int     APPROVE     = JFileChooser.APPROVE_OPTION;
+    /** 
+     * Represents the operator's choice of Cancel 
+     * in the file chooser dialog.
+     * @see #getLastAction()
+     */
+    public static final int     CANCEL      = JFileChooser.CANCEL_OPTION;
+
+    /** The JFileChooser used in this object. */
+    private final JFileChooser  chooser;
+    /** 
+     * The last file successfully processed; null if none.
+     * Set to null if an I/O error occurs during processing.
+     * @see #close()
+     */
+    private File                currFile    = null;
+    /**
+     * The status of the last result obtained during an I/O operation,
+     * true for success, false for failure.
+     * @see #getLastResult()
+     */
+    private boolean             lastResult  = false;
+    /** 
+     * Indicates the last choice selected by the operator when presented
+     * with the JFileChooser dialog. Will always be a value of APPROVE
+     * or CANCEL. Value undetermined if the JFileChooser has not been
+     * posted.
+     * @see #getLastAction()
+     */
+    private int                 lastAction  = CANCEL;
     
     /**
      * Constructor.
@@ -27,65 +59,142 @@ public class ProfileFileManager
         chooser = new JFileChooser( baseDir );
     }
     
+    /**
+     * Gets the JFileChooser encapsulated in this object.
+     * 
+     * @return  the JFileChooser encapsulated in this object
+     */
     public JFileChooser getFileChooser()
     {
         return chooser;
     }
     
+    /**
+     * Returns the file that was the target
+     * of the last operation,
+     * of null if the last operation failed.
+     * 
+     * @return  the target of the last operation, or null if failed
+     */
     public File getCurrFile()
     {
         return currFile;
     }
     
+    /**
+     * Return the result of the last file I/O operation,
+     * true for success or false for failure.
+     * 
+     * @return  the result of the last file I/O operation
+     */
     public boolean getLastResult()
     {
         return lastResult;
     }
     
+    /**
+     * Indicate the last choice made by the operator
+     * when presented with the JFileChooser, either {@link #APPROVE}
+     *  or {@link #CANCEL}.
+     * 
+     * @return  the last option used to terminate the JFileChooser
+     */
+    public int getLastAction()
+    {
+        return lastAction;
+    }
+    
+    /**
+     * Closes the currently open file, if any.
+     */
     public void close()
     {
         currFile = null;
     }
     
+    /**
+     * Populates a newly instantiated Profile object
+     * from a file of the operator's choice,
+     * updating the {@link #lastAction} property.
+     * If the operation is not canceled
+     * the {@link #lastResult} and {@link #currFile}
+     * properties are updated.
+     *
+     * @return  reference to Profile on success, null on failure
+     * 
+     * @see #open(File, Profile)
+     */
     public Profile open()
     {
-        lastResult = false;
         Profile profile = null;
-        int result  = chooser.showOpenDialog( null );
-        if ( result == JFileChooser.APPROVE_OPTION )
+        lastAction = chooser.showOpenDialog( null );
+        if ( lastAction == APPROVE )
             profile = open( chooser.getSelectedFile() );
         return profile;
     }
     
+    /**
+     * Populates the given Profile
+     * from a file of the operator's choice,
+     * updating the {@link #lastAction} property.
+     * If the operation is not canceled
+     * the {@link #lastResult} and {@link #currFile}
+     * properties are updated.
+     * 
+     * @param profile   the given Profile
+     * 
+     * @return  reference to profile on success, null on failure
+     * 
+     * @see #open(File, Profile)
+     */
     public Profile open( Profile profile )
     {
-        lastResult = false;
-        int result  = chooser.showOpenDialog( null );
-        if ( result == JFileChooser.APPROVE_OPTION )
+        lastAction = chooser.showOpenDialog( null );
+        if ( lastAction == APPROVE )
             profile = open( chooser.getSelectedFile(), profile );
         return profile;
     }
     
+    /**
+     * Parse the given file
+     * into a newly instantiate Profile object.
+     * 
+     * @param file  the given file
+     * 
+     * @return  reference to Profile on success, null on failure
+     * 
+     * @see #open(File, Profile)
+     */
     public Profile open( File file )
     {
         Profile profile = open( file, new Profile() );
         return profile;
     }
     
+    /**
+     * Parse the given file into the given Profile.
+     * On completion,
+     * lastResult and currFile are updated.
+     * 
+     * @param file      the given file
+     * @param profile   the given Profile
+     * 
+     * @return  reference to profile on success, null on failure
+     */
     public Profile open( File file, Profile profile )
     {
-        Stream<String>  lines   = null;
         currFile = null;
+        lastResult = false;
         try ( 
             FileReader fReader = new FileReader( file );
             BufferedReader  bReader = new BufferedReader( fReader );
         )
         {
-            lines = bReader.lines();
+            Stream<String>  lines = bReader.lines();
             ProfileParser   parser  = new ProfileParser( profile );
             parser.loadProperties( lines );
-            profile = parser.getProfile();
             currFile = file;
+            lastResult = true;
         }
         catch ( IOException exc )
         {
@@ -101,16 +210,30 @@ public class ProfileFileManager
                 JOptionPane.ERROR_MESSAGE
             );
         }
-        lastResult = profile != null;
         return profile;
     }
     
+    /**
+     * Creates a Profile
+     * and writes it to
+     * a file of the operator's choice.
+     * 
+     * @return  true for success, false for fail
+     */
     public boolean newFile()
     {
-        Profile profile = new Profile();
-        saveAs( profile );
+        saveAs();
         return lastResult;
     }
+    
+    /**
+     * Instantiates a new Profile
+     * and writes it to the given file.
+     * 
+     * @param file  the given file
+     * 
+     * @return  true for success, false for failure
+     */
     public boolean save( File file )
     {
         save( new Profile(), file );
@@ -125,16 +248,17 @@ public class ProfileFileManager
      * this operation is equivalent to {@link #saveAs(Profile)}.
      * Returns true on successful completion
      * false for failure.
-     * If the operator cancels the operation
-     * the state of the object remains unchanged.
      * 
-     * @param file
-     * @return
+     * @param profile   the given Profile
+     * 
+     * @return  true on successful completion false for failure.
      */
     public boolean save( Profile profile )
     {
-        lastResult = currFile == null ? 
-            saveAs( profile ) : save( profile, currFile );
+        if ( currFile == null )
+            saveAs( profile );
+        else
+            save( profile, currFile );
         return lastResult;
     }
     
@@ -157,19 +281,23 @@ public class ProfileFileManager
     
     /**
      * Saves the given Profile to a file
-     * of the operator's choice.
+     * of the operator's choice,
+     * updating the {@link #lastAction} property.
+     * If not canceled by the operator,
+     * updates the {@link #lastResult} and {@link #currFile}
+     * properties.
      * Returns true on success,
      * false otherwise.
      * 
-     * @param profile
+     * @param profile   the given Profile
      * 
      * @return  true on success, false otherwise
      */
     public boolean saveAs( Profile profile )
     {
-        int result  = chooser.showSaveDialog( null );
-        if ( result == JFileChooser.APPROVE_OPTION )
-            lastResult = save( profile, chooser.getSelectedFile() );
+        lastAction = chooser.showSaveDialog( null );
+        if ( lastAction == APPROVE )
+            save( profile, chooser.getSelectedFile() );
         return lastResult;
     }
     
