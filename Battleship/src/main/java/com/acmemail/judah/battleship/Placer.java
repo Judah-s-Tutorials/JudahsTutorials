d28a2c9ac007e9244b4bc4667dc721921e3cd2dd
John Straub <johnstraub1954@gmail.com>
2026-03-25 15:47:16 -0600

    work on Battlesip project

--- Battleship/src/main/java/com/acmemail/judah/battleship/Placer.java
+++ Battleship/src/main/java/com/acmemail/judah/battleship/Placer.java
@@ -8,39 +8,108 @@ import javax.swing.JOptionPane;
 /**
  * An instance of this class is used to request a ship configuration
  * from the operator.
+ * It is designed to be the sole source of interaction with the operator
+ * for this purpose.
+ * <p>
+ * This class is designed to be used with Mockito
+ * in order to improve testability of operator interaction.
+ * To that end:
+ * <ol>
+ * <li>All operator interaction is conducted via JOptionPane.</li>
+ * <li>
+ *     Access to JOptionPane is provided by 
+ *     interface {@linkplain JOptionPaneI}.
+ * </li>
+ * <li>
+ * 
+ * <li>
+ *     During production,
+ *     the functionality of JOPtionPaneI is provided by
+ *     a concrete class which calls directly into JOptionPane.
+ * </li>
+ * <li>
+ *     During testing,
+ *     the functionality of JOPtionPaneI is provided by
+ *     class which "mocks" JOptionPane.
+ *     JOptionPane.showInputDialog, for example,
+ *     is replaced by a method that returns a string
+ *     provided by the testing facility,
+ *     and which does not display a physical dialog.
+ * </li>
+ * </ol>
+ * <p>
+ * A single instance of this class can be used
+ * to place multiple ships, 
+ * for example:
+ * <pre style="margin-left:2em;">Placer placer = new Placer();
+ * placer.placeShip();
+ * placer.placeShip();
+ * placer.placeShip();</pre>
+ * 
+ * @author Jack
+ * 
+ * @see JOptionPaneI
+ * @see JOptionPaneS
+ * @see The <a href="https://site.mockito.org/">Mockito Website</a>
+ * @see #setJOptionPaneInterface(JOptionPaneI)
+ * @see #getJOptionPaneInterface()
  */
 public class Placer
 {
+    /**  
+     * The interface to JOptionPane.
+     * @see #setJOptionPaneInterface(JOptionPaneI)
+     */
     private static JOptionPaneI jOptionPane = new JOptionPaneS();
-    static final String title   = "Place Ship";
+    /** The title of all dialogs that interact with the operator. */
+    private static final String title   = "Place Ship";
 
+    /**  The ShipType of the ship to place. */
     private ShipType    type;
+    /**  The grid coordinates of the ship to place. */
     private GridCoords  coords;
+    /**  The orientation of the ship to place. */
     private Orientation orientation;
     
+    /**
+     * Gets the implementation of JOptionPaneI
+     * currently being used to interact with the operator.
+     * 
+     * @return  the JOptionPaneI implementation currently in use
+     */
     public static JOptionPaneI getJOptionPaneInterface()
     {
         return jOptionPane;
     }
     
+    /**
+     * Sets the implementation of JOptionPaneI
+     * that is used to provide operator input
+     * to the given object.
+     * 
+     * @param jOptionPane   the given object
+     */
     public static void setJOptionPaneInterface( JOptionPaneI jOptionPane )
     {
         Placer.jOptionPane = jOptionPane;
     }
 
     /**
-     * 
+     * The default constructor; not used.
      */
     public Placer()
     {
-//        this( new JOptionPaneS() );
     }
-    
-//    public Placer( JOptionPaneI jOptionPane )
-//    {
-//        this.jOptionPane = jOptionPane;
-//    }
 
+    /**
+     * Interacts with the operator to configure a ship
+     * to be added to the fleet/grid.
+     * The configured ship is returned to the caller.
+     * The operator can cancel the operation at any step,
+     * in which case null is returned to the caller.
+     * 
+     * @return  the configured ship, or null, if the operation is canceled
+     */
     public Ship placeShip()
     {
         type = null;
@@ -57,6 +126,20 @@ public class Placer
         return ship;
     }
     
+    /**
+     * Displays a dialog containing a list
+     * of all available ship types,
+     * and allows the operator to select one.
+     * The selected type is returned to the caller
+     * unless the operation is canceled,
+     * in which case null is returned.
+     *  
+     * @return the ship type selected by the operator, or null if canceled
+     * 
+     * @see ShipType
+     * @see ShipType()
+     * @see ShipType#registerDefaultTypes()
+     */
     private static ShipType getType()
     {
         final String    noTypes     = "No ship types to select";
@@ -85,6 +168,27 @@ public class Placer
         return (ShipType)option;
     }
     
+    /**
+     * Parses a string containing row/column coordinates,
+     * validates the coordinates,
+     * and converts them into x- and y-coordinates.
+     * if possible, a GridCoords object containing the x- and
+     * y-coordinates is returned to the caller.
+     * Null is returned 
+     * if the input cannot be successfully parsed and converted.
+     * For a description of the valid formats
+     * for the row/column coordinates 
+     * see {@linkplain Label#parseRowCol(String)}.
+     * 
+     * @param strCoords the row/column coordinates to parse
+     * 
+     * @return
+     *      a GridCoords object containing the converted x- and
+     *      y-coordinates, or null if the input cannot be parsed
+     *      
+     * @see Label
+     * @see Label#parseRowCol(String)
+     */
     private static GridCoords parseCoords( String strCoords )
     {
         String          worker      = strCoords.trim().toUpperCase();
@@ -116,6 +220,17 @@ public class Placer
         return coords;
     }
     
+    /**
+     * Displays a dialog containing the
+     * two possible options for configuring
+     * the orientation of a ship,
+     * allow the operator to choose one.
+     * The chosen orientation is returned
+     * unless the operator cancels the operation,
+     * in which case null is returned.
+     * 
+     * @return  the selected orientation or null if canceled
+     */
     private static Orientation getOrientation()
     {
         final String        prompt      = "Select Orientation";
@@ -141,6 +256,35 @@ public class Placer
         return orientation;
     }
     
+    /**
+     * Prompts the operator for the grid coordinates
+     * of the position of the new ship,
+     * then creates the ship. 
+     * The coordinates entered by the operator
+     * are validated;
+     * if not valid,
+     * an error message is displayed
+     * and the operator is repeatedly prompted
+     * to enter coordinates
+     * until valid coordinates are entered
+     * or the operation is canceled.
+     * The created ship is returned
+     * unless the operation is canceled,
+     * in which case null is returned.
+     * Valid coordinates are converted to
+     * x- and y- grid coordinates
+     * and stored in {@linkplain #coords}.
+     * <p>
+     * Precondition:
+     * the type of the ship has been determined
+     * and stored in {@linkplain #type}.
+     * <p>
+     * Precondition:
+     * the orientation of the ship has been determined
+     * and stored in {@linkplain #orientation}.
+     * 
+     * @return  the created ship, or null if the operation is canceled
+     */
     private Ship getShip()
     {
         final String    prompt      = "Enter Ship Coordinates";
@@ -176,6 +320,15 @@ public class Placer
         return ship;
     }
     
+    /**
+     * Displays a dialog containing a given message 
+     * list of errors,
+     * and waits for the operator
+     * to dismiss the dialog.
+     * 
+     * @param message   the given message
+     * @param errors    the given list of errors
+     */
     private static 
     void showErrorMessage( String message, List<String> errors )
     {
